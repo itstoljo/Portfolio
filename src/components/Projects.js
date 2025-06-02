c5af9f800c18cc94ae5a335e152d9b12cf0b566e
itstoljo <150053011+itstoljo@users.noreply.github.com>
2025-06-02 23:17:07 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -32,7 +32,7 @@ function Projects() {
             old plug-ins with download links]
           </p>
           <a
-            href="https://bedroomproducersblog.com/
+            href="https://bedroomproducersblog.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
